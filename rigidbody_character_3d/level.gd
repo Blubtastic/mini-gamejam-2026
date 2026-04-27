782a8c430e61b5852f486155ba1059e3c634a0d8
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-04-27 08:08:11 +0200

    feat: move rigidbody into folder

--- rigidbody_character_3d/level.gd
+++ rigidbody_character_3d/level.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 # Random spawn of Rigidbody cubes.
 func _on_SpawnTimer_timeout() -> void:
-	var new_rb: RigidBody3D = preload("res://cube_rigidbody.tscn").instantiate()
+	var new_rb: RigidBody3D = preload("res://rigidbody_character_3d/cube_rigidbody.tscn").instantiate()
 	new_rb.position.y = 15
 	new_rb.position.x = randf_range(-5, 5)
 	new_rb.position.z = randf_range(-5, 5)
