eccfb861eb77257d4de27369a9e72f61a33ec4c7
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-05-01 14:35:18 +0200

    feat: pretty end UI

--- main.gd
+++ main.gd
@@ -2,12 +2,14 @@ extends Node3D
 const UI_END = preload("uid://0c1myc43nfbv")
 
 @export var end: Area3D
+var has_ended := false
 
 func _ready() -> void:
 	end.body_entered.connect(on_end_entered)
 
 
 func on_end_entered(body: Node3D) -> void:
-	if body is RigidBody3D and body is Player:
+	if body is RigidBody3D and body is Player and !has_ended:
+		has_ended = true
 		var instance := UI_END.instantiate()
 		add_child(instance)
