345c0d83707e592e2b716aa02e2f35d0a1bf1c89
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-05-06 14:46:47 +0200

    feat: more fancy button mesh

--- components/scripts/button.gd
+++ components/scripts/button.gd
@@ -3,8 +3,8 @@ extends Area3D
 var is_pressed := false
 var pressed_position := Vector3(0, -0.5, 0)
 @onready var poof: GPUParticles3D = $Poof
-@onready var button_mesh: MeshInstance3D = $Button
 @export var thing_to_open: Node3D
+@onready var button_mesh: Node3D = $Button
 
 
 func _on_body_entered(body: Node3D) -> void:
