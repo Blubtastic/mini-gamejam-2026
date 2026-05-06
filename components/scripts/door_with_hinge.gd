aac6aef50a1c299981ed5c248723843819d204ed
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-05-06 09:44:03 +0200

    refactor: code cleanup

--- components/scripts/door_with_hinge.gd
+++ components/scripts/door_with_hinge.gd
@@ -2,10 +2,8 @@ extends Node3D
 
 
 @onready var pushable_door: RigidBody3D = $PushableDoor
-
 ## The door can't be moved if it is frozen.
 @export var is_frozen := false
-
 ## If set to anything else than 0, picking up a card with this ID unlocks the door.
 @export var open_by_card_id := 0
 
@@ -15,6 +13,7 @@ func _ready() -> void:
 
 func open() -> void:
 	pushable_door.freeze = false
+
 
 # Open door if player is close and has correct card
 func _on_detect_player_body_entered(body: Node3D) -> void:
